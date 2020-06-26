31f9e9dd014852d5cd59fcb76fc2ceb3652451b2
Naeem Masnadi <nmasnadi@gmail.com>
2020-06-26 19:00:43 -0400

    fixed error messaging

--- app.py
+++ app.py
@@ -1,45 +1,38 @@
-from flask import Flask, render_template, request, redirect
-from helper import read_data, make_plot
-# from bokeh.embed import json_item
+from flask import Flask, render_template, request
+import helper
 from bokeh.embed import components
 from bokeh.resources import CDN
-from bokeh.resources import INLINE
 import numpy as np
 import json
-from jinja2 import Template
 
 app = Flask(__name__)
 
-ticker_list = ["GOOG", "IBM", "MSFT"]
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    current_ticker = request.args.get("ticker_symbol")
-    print(current_ticker)
-    if current_ticker == None:
-        current_ticker = "GOOG"
-    a = read_data(current_ticker, data_size = 'short')
-    x = np.array(a.index, dtype = np.datetime64)
-    y = a['4. close'].to_numpy()
-    p = make_plot(x, y)
-    script, div = components(p)
-    return render_template("index2.html", script = script, div = div, \
-    ticker_list = ticker_list, current_ticker = current_ticker, \
-    resources = CDN.render())
 
-# @app.route('/plot')
-# def plot():
-#     a = read_data(data_size = 'full')
-#     x = np.array(a.index, dtype = np.datetime64)
-#     y = a['4. close'].to_numpy()
-#     p = make_plot(x, y)
-#     script, div = components(p)
-#     return render_template("index2.html", script = script, div = div)
-    # return json.dumps(json_item(p, "myplot"))
-
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
+    if request.method == 'POST':
+        current_ticker = request.form['ticker_symbol']
+        ticker_data, error, ticker_list = helper.read_data(current_ticker, num_days = 30)
+        if len(error) == 0:
+            p = helper.make_plot(ticker_data, current_ticker)
+            script, div = components(p)
+            return render_template("index2.html", script = script, div = div, \
+            current_ticker = current_ticker, \
+            resources = CDN.render())
+        else:
+            script = "Error!"
+            # script = """
+            # <h3>Error!</h3>
+            # <br>
+            # <h4>{}</h4>
+            # <br>
+            # <h3>Best matches:</h3>
+            # <br>
+            # <h4>{}</h4>
+            # """.format(error, ", ".join(ticker_list))
+            return render_template("index2.html", script = script, resources = CDN.render())
+    else:
+        return render_template("index2.html")#, resources = CDN.render())
 
 if __name__ == '__main__':
     app.run(port=33507)
--- helper.py
+++ helper.py
@@ -4,11 +4,11 @@ import requests
 from bokeh.plotting import figure, show
 import json
 
-def read_data(tickerSymbol = "GOOG", data_size = 'short'):
 
+def read_data(tickerSymbol, num_days = 30):
+    # obtain data from AlphaVantage API
     alphaKey = 'NB9OQA12MMYF402C'
-
-    if data_size == 'short':
+    if num_days < 100:
         url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+\
         tickerSymbol + '&apikey=' + alphaKey
     else:
@@ -17,25 +17,60 @@ def read_data(tickerSymbol = "GOOG", data_size = 'short'):
 
     r = requests.get(url)
     data_json = json.loads(r.text)
-    data_pd = pd.DataFrame(data_json['Time Series (Daily)'], dtype = np.float64).T
-    data_pd.index = pd.to_datetime(data_pd.index)
-    return data_pd
+    # print(data_json)
+    if "Error Message" in data_json:
+        print(data_json)
+        error = data_json["Error Message"]
+        url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' +\
+        tickerSymbol + '&apikey=' + alphaKey
+        r = requests.get(url)
+        data_json = json.loads(r.text)
+        try:
+            ticker_list = [s['1. symbol'] for s in data_json['bestMatches']]
+        except:
+            ticker_list = []
+        data_pd = None
+    elif "Note" in data_json:
+        error = data_json["Note"]
+        data_pd = None
+        ticker_list = []
+    elif 'Time Series (Daily)' in data_json:
+        error = ""
+        ticker_list = []
+        data_pd = pd.DataFrame(data_json['Time Series (Daily)'], dtype = np.float64).T
+        data_pd.index = pd.to_datetime(data_pd.index)
+        # slice the data for the inquired dates
+        start =  np.datetime64('today') - np.timedelta64(num_days, 'D')
+        data_pd = data_pd[data_pd.index >= start]
+    else:
+        error = "Something went wrong! Please try again"
+
+    return data_pd, error, ticker_list
 
 
-def make_plot(x, y):
-    p = figure(
+def make_plot(data, tickerSymbol = "GOOG"):
+    x = np.array(data.index, dtype = np.datetime64)
+    y = data['4. close'].to_numpy()
+
+    p = figure(title = tickerSymbol,
        tools="pan,box_zoom,reset,save",
-       plot_width=800, plot_height=350, x_axis_type="datetime"
+       plot_width=800, plot_height=450, x_axis_type="datetime"
     )
+    p.title.align = "center"
+    p.title.text_font_size = "25px"
     p.xaxis.axis_label = "date"
+    p.xaxis.axis_label_text_font_size = "25px"
     p.yaxis.axis_label = "price"
-    p.circle(x, y, color="darkgray", fill_alpha=0.5, size=5)
-    p.line(x, y, color = 'black')
+    p.yaxis.axis_label_text_font_size = "25px"
+
+    p.circle(x, y, color="darkgray", fill_alpha=0.5, size=10)
+    p.line(x, y, color = 'black', line_width = 2)
     return p
 
 if __name__ == '__main__':
-    a = read_data(data_size = 'full')
-    x = np.array(a.index, dtype = np.datetime64)
-    y = a['4. close'].to_numpy()
-    p = make_plot(x, y)
+    a, error, tlist = read_data("GOOG", num_days = 30)
+    print(a)
+    # x = np.array(a.index, dtype = np.datetime64)
+    # y = a['4. close'].to_numpy()
+    p = make_plot(a)
     show(p)
